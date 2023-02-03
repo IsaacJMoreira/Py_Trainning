6a11c191cb88cdb5a9c4c63a5b635e6e28332b66
Isaac J Moreira <120274547+IsaacJMoreira@users.noreply.github.com>
2023-02-03 16:45:52 -0300

    further improvemennt

--- Image_Converter/Image_Converter.py
+++ Image_Converter/Image_Converter.py
@@ -1,14 +1,15 @@
 from PIL import Image, ImageOps
+import os #imports the ability to navegate through the file siste
 '''
 This simple program converts an image to an image made of ascii characters.
 This version is very crude. Some refacturing must be done
 
 TO DO:
 
-1 - make it possible for to output a char array
+DONE! 1 - make it possible for to output a char array
 2 - make this entire thing into a file to be included in another .py file
 3 - the function must accept the resizing divider and the name of the file as parameters
-4 - make another function that is able to print this to a .txt
+DONE! 4 - make another function that is able to print this to a .txt
 
 current version: 0.1
 date: 31/01/2023
@@ -25,28 +26,41 @@ while(tryAgain):
         print("Image dimensions: " + str(img.size))#print the tuple that holds the dimentions of the object
         #print(img.format)#print the format
         print(img.format_description)#print the discription of the format ex: JPEG (ISO 10918)
-        tryScalingAgain = True
-        while(tryScalingAgain):
+        askAgain = True
+        while(askAgain):
             try:
                 downSizer = int(input("We need to downsize the image. Type the Amount: "))
-                BIT = img.resize(tuple(int(t/downSizer) for t in img.size))
-                BIT = BIT.convert("L")
-                BIT = ImageOps.invert(BIT)
-                tryScalingAgain = False
+                img = img.resize(tuple(int(t/downSizer) for t in img.size))
+                img = img.convert("L")
+                img = ImageOps.invert(img)
+                askAgain = False
             except:
                 print("Sorry, invalid value typed. Please try again.")
-                tryScalingAgain = True
+                askAgain = True
 
-        print(imgFile + " was downscaled to: " + str(BIT.size) + "\n")
+        askAgain = True
+        while(askAgain):
+            try:
+                fileName = input("Give the text output file a name : ") + ".txt"
+                TextFile = open(fileName, 'w')
+                print("File openned successfully.")
+                askAgain = False
+            except:
+                print("Unable to name a file with: '" + fileName + "'. Please again.")
+                askAgain = True
+
+        print(imgFile + " was downscaled to: " + str(img.size) + "\n")
 
 
 
-        singlePixel = BIT.load() # we load the BIT object into Pixel, making it a PixelAccess object
+        singlePixel = img.load() # we load the BIT object into Pixel, making it a PixelAccess object
 
-        for i in range(BIT.size[1]):# here we generate the image out of chars
-            for j in range(BIT.size[0]):
-                print(imageMap[int(singlePixel[j, i]/len(imageMap))], end = '')     
-            print()
+        for i in range(img.size[1]):# here we generate the image out of chars
+            for j in range(img.size[0]):
+                TextFile.write(imageMap[int(singlePixel[j, i]/len(imageMap))])     
+            TextFile.write("\n")
+    
+        TextFile.close()
         
         print("Conversion is done.")
         tryAgain = False
---
+++ Image_Converter/Text_Saver.py
@@ -0,0 +1,9 @@
+import os
+
+
+
+firstFile = open("First.txt", 'w')
+firstFile.write("Isaac Moreira")
+firstFile.close()
+
+os.startfile("First.txt")
