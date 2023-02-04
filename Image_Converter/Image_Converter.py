999921133f7361f573a5c4578d4c8fa0b1a6f700
Isaac J Moreira <120274547+IsaacJMoreira@users.noreply.github.com>
2023-02-04 04:57:07 -0300

    changed the name

--- Image_Converter/Image_Converter.py
+++ Image_Converter/Image_Converter.py
@@ -1,75 +1,77 @@
 from PIL import Image, ImageOps
-import os #imports the ability to navegate through the file siste
+import cv2
+import os #imports the ability to navegate through the file sistem
 '''
 This simple program converts an image to an image made of ascii characters.
 This version is very crude. Some refacturing must be done
 
-TO DO:
-
-DONE! 1 - make it possible for to output a char array
-2 - make this entire thing into a file to be included in another .py file
-3 - the function must accept the resizing divider and the name of the file as parameters
-DONE! 4 - make another function that is able to print this to a .txt
-
-current version: 0.1
+current version: 0.35
 date: 31/01/2023
 '''
+Downscale = 3
+imageMap =  ("   ", " . ", ". .", "...", ".:.", ":.:", ":::", ":|:", "|:|", "|||", "|#|", "#|#", "###", "#$#", "$#$", "$$$")#map for converting and image to asci. Add as much as you need
 
-imageMap = ("   ", " . ", ". .", "...", ".:.", ":.:", ":::", ":|:", "|:|", "|||", "|#|", "#|#", "###", "#$#", "$#$", "$$$")#map for converting and image to asci. Add as much as you need
+#try using the map below
+'''imageMap2 = ("   ", " . ", ". .", "...",".,.",",.,",",,,",",-,","-,-","---","-~-","~-~","~~~","~:~",":~:",":::",":;:",":;:",";;;",
+             ";=;","=;=","===","=!=","!=!","!!!","###","#$#","$#$", "$$$","$@$","@$@","@@@")'''
+
+
 tryAgain = True
+webCam = cv2.VideoCapture(0)#creates a webCam object
+exit = False
 
-while(tryAgain):
-    imgFile = input("\nEnter the image file name with the extension. (.jpg, .jpeg, .png, etc): ")
+while True:
+
+    successful, frame = webCam.read()#creates the webCam object and a flag to signal the successin reading the data from Webcam
+
+    if not successful:#if an error had acurred, exit with a message
+        print("Unable to access the WebCam.")
+        break
+        
+    cv2.imshow("Press SPACE to take a picture.", frame)#open a window that prompts you to press SPACE
+
+    key = cv2.waitKey(1)#Waits a key press
+
+    if key%256 == 27:#waits esc key
+        print("Closing the app.")
+        exite = True
+        break
+    elif key%256 == 32:#takes a picture if space is pressed
+        print("Photo Taken.")
+        cv2.imwrite("Frame_1.png", frame)
+        exit = False #belts, bracers and suspenders
+        break
+if not exit:
     try:
-        img = Image.open(imgFile)
+        img = Image.open("Frame_1.png")
         print("Image loaded successfully.")
         print("Image dimensions: " + str(img.size))#print the tuple that holds the dimentions of the object
         #print(img.format)#print the format
         print(img.format_description)#print the discription of the format ex: JPEG (ISO 10918)
         askAgain = True
-        while(askAgain):
-            try:
-                downSizer = int(input("We need to downsize the image. Type the Amount: "))
-                img = img.resize(tuple(int(t/downSizer) for t in img.size))
-                img = img.convert("L")
-                img = ImageOps.invert(img)
-                askAgain = False
-            except:
-                print("Sorry, invalid value typed. Please try again.")
-                askAgain = True
+        
+                    
+        img = img.resize(tuple(int(t/Downscale) for t in img.size))#resizes the image
+        img = img.convert("L")#converts it to gray scale
+        img = ImageOps.invert(img)#inverts the image
+                    
 
-        askAgain = True
-        while(askAgain):
-            try:
-                fileName = input("Give the text output file a name : ") + ".txt"
-                TextFile = open(fileName, 'w')
-                print("File openned successfully.")
-                askAgain = False
-            except:
-                print("Unable to name a file with: '" + fileName + "'. Please again.")
-                askAgain = True
-
-        print(imgFile + " was downscaled to: " + str(img.size) + "\n")
-
-
-
+        
+        TextFile = open("Picture_to_ASCII_1.txt", 'w')#Creates the
+        print("File openned successfully.")
         singlePixel = img.load() # we load the BIT object into Pixel, making it a PixelAccess object
 
         for i in range(img.size[1]):# here we generate the image out of chars
-            for j in range(img.size[0]):
-                TextFile.write(imageMap[int(singlePixel[j, i]/len(imageMap))])     
-            TextFile.write("\n")
-    
-        TextFile.close()
+            for w in range(2):
+                for j in range(img.size[0]):
+                    TextFile.write(imageMap[int(singlePixel[j, i]/(256/len(imageMap)))])
+                TextFile.write("\n")
         
+        TextFile.close()
+            
         print("Conversion is done.")
         tryAgain = False
         #BIT.show()
     except:
-        print("Sorry, no file named " + imgFile + " was found.")
-        print("\nPlease try again.")
-        tryAgain = True
-
-
-
-
+        print("Sorry, unable to find the picture taken")
+        print("\nEnding prgram.")
--- Image_Converter/Text_Saver.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-
-firstFile = open("First.txt", 'w')
-firstFile.write("Isaac Moreira")
-firstFile.close()
-
-os.startfile("First.txt")
